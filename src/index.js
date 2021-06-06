cdb871e87a240e6b17b2dd90416407397e53a2a3
Pou Lin <poumason@live.com>
2021-06-06 17:35:51 +0800

    add test code to create radar record

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 const ProductsAPI = require('../SharedCode/db/products_table_api');
 const SellerAPI = require('../SharedCode/db/seller_api');
 const RutenAPI = require('../SharedCode/ruten_api');
+const RadarAPI = require('../SharedCode/db/radar_table_api');
+const ChatAPI = require('../SharedCode/db/chat_api');
 const { isAvailablePrice } = require('../SharedCode/utility');
 
 async function main () {
   const rutenAPI = new RutenAPI();
   const productsAPI = new ProductsAPI();
   const sellerAPI = new SellerAPI();
+  const radarAPI = new RadarAPI();
+  const chatAPI = new ChatAPI();
   const sellers = await sellerAPI.getSellers();
 
   const newProducts = [];
@@ -56,7 +60,26 @@ async function main () {
 
     if (!exist || exist.records.length === 0) {
       // add new item
-      await productsAPI.createProduct(newItem);
+      const addedResult = await productsAPI.createProduct(newItem);
+
+      if (!addedResult || addedResult.records.length === 0) {
+        // add item failed.
+        continue;
+      }
+
+      const addedItem = addedResult.records[0];
+      const chatsResult = await chatAPI.getChats();
+
+      for (const chat of chatsResult.records) {
+        const radarResult = await radarAPI.createRadar({
+          fields: {
+            Products: [addedItem.id],
+            Charts: [chat.id]
+          }
+        });
+        console.log(radarResult);
+      }
+
       console.log(`create item: ${newItem.name}`);
     }
   }
